21f961dff623053b889787eb3d261af9501cb145
GA3RlEL <kruszkagabrys@gmail.com>
2024-06-27 16:55:16 +0200

    benefits with footer sections are added with styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
 })
 export class AppComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { TrustedLogosComponent } from './trusted-logos/trusted-logos.component';
+import { BenefitsComponent } from './benefits/benefits.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [HeaderComponent, HeroSectionComponent, TrustedLogosComponent],
+  imports: [
+    HeaderComponent,
+    HeroSectionComponent,
+    TrustedLogosComponent,
+    BenefitsComponent,
+    FooterComponent,
+  ],
 })
 export class HomeComponent {}
